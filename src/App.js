033703371c8def982233758f54dbb479bb7849c6
tharuuj <it22257840@my.sliit.lk>
2024-04-18 13:14:33 +0530

    changes in frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Headers/Headers";
+import HomeEmp from "./Containers/EmployeePersonal/HomeEmp";
+import Register from "./Containers/EmployeePersonal/RegisterEmp";
+import Edit from "./Containers/EmployeePersonal/EditEmp";
+import Profile from "./Containers/EmployeePersonal/ProfileEmp";
+
+import HomeWorkHistory from "./Containers/WorkHistory/HomeWorkHistory"
+import AddWorkHistory from "./Containers/WorkHistory/AddWorkHistory"
+import ViewWorkHistory from "./Containers/WorkHistory/ViewWorkHistory"
+import UpdateWorkHistory from "./Containers/WorkHistory/UpdateWorkHistory"
+
+import HomeAttendance from "./Containers/Attendance/HomeAttendance"
+import AddAttendance from "./Containers/Attendance/AddAttendance"
+import ViewAttendance from "./Containers/Attendance/ViewAttendance"
+import UpdateAttendance from "./Containers/Attendance/UpdateAttendance"
+
+import { Route, Routes } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>      
+      <Routes>
+        
+        <Route path='/homeemp' element={<HomeEmp />} />
+        <Route path='/registeremp' element={<Register />} />
+        <Route path='/editemp/:id' element={<Edit />} />
+        <Route path='/empprofile/:id' element={<Profile />} />
+
+        <Route path='/addwork' element={<AddWorkHistory />} />
+        <Route path='/workhistory' element={<HomeWorkHistory />} />
+        <Route path='/viewworkhistory/:id' element={<ViewWorkHistory />} />
+        <Route path='/updateworkhistory/:id' element={<UpdateWorkHistory />} />
+
+        <Route path='/attendance' element={<HomeAttendance />} />
+        <Route path='/addattendance' element={<AddAttendance />} />
+        <Route path='/viewattendance/:id' element={<ViewAttendance />} />
+        <Route path='/updateatt/:id' element={<UpdateAttendance />} />
+      </Routes>
+    
+      
+    </>
   );
 }
 
